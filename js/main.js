fcbc26e50e71ffa6ecfa6766932e533af822b257
Hi.Kendal <kendalj5@gmail.com>
2019-08-16 10:35:27 -0400

    Hi

--- js/main.js
+++ js/main.js
@@ -3,10 +3,6 @@ $(document).ready(function() {
 
   'use strict';
 
-  // ========================================================================= //
-  //  //SMOOTH SCROLL
-  // ========================================================================= //
-
 
   $(document).on("scroll", onScroll);
 
@@ -46,9 +42,7 @@ $(document).ready(function() {
     }
   }
 
-  // ========================================================================= //
-  //  //NAVBAR SHOW - HIDE
-  // ========================================================================= //
+
 
 
   $(window).scroll(function() {
@@ -79,79 +73,10 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["HI THERE", "I'M KENDAL", "MY PORTFOLIO IS ALMOST COMPLETE", "FOR NOW...", "HERE ARE SOME LINKS", "ENJOY"],
+      strings: ["HI THERE", "ENJOY"],
       typeSpeed: 100,
       loop: true,
     });
   });
 
-
-  // ========================================================================= //
-  //  Owl Carousel Services
-  // ========================================================================= //
-
-
-  $('.services-carousel').owlCarousel({
-      autoplay: true,
-      loop: true,
-      margin: 20,
-      dots: true,
-      nav: false,
-      responsiveClass: true,
-      responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
-    });
-
-  // ========================================================================= //
-  //  magnificPopup
-  // ========================================================================= //
-
-  var magnifPopup = function() {
-    $('.popup-img').magnificPopup({
-      type: 'image',
-      removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
-      gallery: {
-        enabled: true
-      },
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
-
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
-
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-          // openerElement is the element on which popup was initialized, in this case its <a> tag
-          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
-      }
-    });
-  };
-
-
-  // Call the functions
-  magnifPopup();
-
 });
-
-// ========================================================================= //
-//  Porfolio isotope and filter
-// ========================================================================= //
-$(window).load(function(){
-
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-thumbnail',
-    layoutMode: 'fitRows'
-  });
-
-  $('#portfolio-flters li').on( 'click', function() {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
-
-    portfolioIsotope.isotope({ filter: $(this).data('filter') });
-  });
-
-})
